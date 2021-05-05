dcbc6230c3f5efa7820dcc286aab6397ae470566
faisal-arshad <76099135+faisal-arshad@users.noreply.github.com>
2021-05-05 13:48:20 +0500

    changes

--- lib/ui/hall_booking.dart
+++ lib/ui/hall_booking.dart
@@ -1,6 +1,9 @@
 import 'package:event_book_app/constants/app_colors.dart';
 import 'package:event_book_app/constants/app_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:intl/intl.dart';
+import 'package:time_range_picker/time_range_picker.dart';
 import 'components/outlined_input_field.dart';
 import 'components/rounded_button.dart';
 
@@ -10,7 +13,8 @@ class HallBooking extends StatefulWidget {
 }
 
 class _HallBookingState extends State<HallBooking> {
-  String currentDate;
+  var dateFormat = new DateFormat('dd-MM-yyyy');
+  String currentDate, startTime, endTime;
   Color tileColor = AppColors.kPrimaryColor;
   int tileValue = 1;
 
@@ -31,10 +35,10 @@ class _HallBookingState extends State<HallBooking> {
 
   getCurrentDate() {
     var date = new DateTime.now().toString();
-
     var dateParse = DateTime.parse(date);
+    var formattedDate = dateFormat.format(dateParse);
 
-    var formattedDate = "${dateParse.day}-${dateParse.month}-${dateParse.year}";
+    //var formattedDate = "${dateParse.day}-${dateParse.month}-${dateParse.year}";
 
     setState(() {
       currentDate = formattedDate.toString();
@@ -242,6 +246,49 @@ class _HallBookingState extends State<HallBooking> {
                       )
                     ],
                   ),
+                  SizedBox(height: 10),
+                  Row(
+                    children: <Widget>[
+                      Flexible(
+                        flex: 2,
+                        fit: FlexFit.tight,
+                        child: OutlinedInputField(
+                            // hintText: currentDate.toString(),
+                            ),
+                      ),
+                      SizedBox(width: 10),
+                      Flexible(
+                        flex: 1,
+                        fit: FlexFit.tight,
+                        child: TextButton.icon(
+                          label: Text(
+                            "Time",
+                            style: TextStyle(
+                              color: AppColors.kWhiteColor,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          icon: Icon(
+                            FontAwesomeIcons.clock,
+                            color: AppColors.kWhiteColor,
+                          ),
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(
+                                AppColors.kPrimaryColorLight),
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                            ),
+                          ),
+                          onPressed: () {
+                            _selectTime(context);
+                          },
+                        ),
+                      )
+                    ],
+                  ),
                 ],
               ),
             ),
@@ -284,15 +331,103 @@ class _HallBookingState extends State<HallBooking> {
     final DateTime pickedDate = await showDatePicker(
         context: context,
         initialDate: dateTime,
+        currentDate: dateTime,
+        initialDatePickerMode: DatePickerMode.day,
+        initialEntryMode: DatePickerEntryMode.calendar,
         firstDate: DateTime(2021),
         lastDate: DateTime(2050));
+
     if (pickedDate != null && pickedDate != dateTime)
       setState(() {
         var dateParse = DateTime.parse(pickedDate.toString());
-        var formattedDate =
-            "${dateParse.day}-${dateParse.month}-${dateParse.year}";
+        var formattedDate = dateFormat.format(dateParse);
         currentDate = formattedDate.toString();
         print(currentDate);
       });
   }
+
+  Future<void> _selectTime(BuildContext context) async {
+    final TimeRangePicker timePicked = showTimeRangePicker(
+      context: context,
+      start: TimeOfDay(hour: 9, minute: 0),
+      end: TimeOfDay(hour: 12, minute: 0),
+      onStartChange: (start) {
+        startTime = start.toString();
+        print("start time " + start.toString());
+      },
+      onEndChange: (end) {
+        endTime = end.toString();
+        print("end time " + end.toString());
+      },
+      disabledTime: TimeRange(
+          startTime: TimeOfDay(hour: 22, minute: 0),
+          endTime: TimeOfDay(hour: 8, minute: 0)),
+      disabledColor: Colors.red.withOpacity(0.5),
+      interval: Duration(minutes: 30),
+      use24HourFormat: false,
+      padding: 30,
+      strokeWidth: 12,
+      handlerRadius: 14,
+      ticks: 12,
+      strokeColor: Colors.orange,
+      handlerColor: Colors.orange[700],
+      selectedColor: Colors.amber,
+      backgroundColor: Colors.black.withOpacity(0.3),
+      ticksColor: Colors.white,
+      snap: true,
+      labels: ["12 pm", "3 am", "6 am", "9 am", "12 am", "3 pm", "6 pm", "9 pm"]
+          .asMap()
+          .entries
+          .map((e) {
+        return ClockLabel.fromIndex(idx: e.key, length: 8, text: e.value);
+      }).toList(),
+      labelOffset: -35,
+      rotateLabels: false,
+      labelStyle: TextStyle(
+          fontSize: 20, color: Colors.black, fontWeight: FontWeight.bold),
+      timeTextStyle: TextStyle(
+          color: Colors.orange[700],
+          fontSize: 22,
+          fontStyle: FontStyle.italic,
+          fontWeight: FontWeight.bold),
+      activeTimeTextStyle: TextStyle(
+          color: Colors.orange,
+          fontSize: 24,
+          fontStyle: FontStyle.italic,
+          fontWeight: FontWeight.bold),
+    );
+
+    print("Hello");
+    if (timePicked != null) {
+      print("Start Time -> " + startTime);
+      print("End Time -> " + endTime);
+    }
+  }
+
+  // Future<void> showTimeRangePicker(BuildContext context) {
+  //   return showDialog(
+  //       context: context,
+  //       builder: (BuildContext context) {
+  //         return AlertDialog(
+  //           title: Text("Choose event time"),
+  //           content: TimeRangePicker(
+  //             initialFromHour: DateTime.now().hour,
+  //             initialFromMinutes: DateTime.now().minute,
+  //             initialToHour: DateTime.now().hour,
+  //             initialToMinutes: DateTime.now().minute,
+  //             backText: "Back",
+  //             nextText: "Next",
+  //             cancelText: "Cancel",
+  //             selectText: "Select",
+  //             onSelect: (from, to) {
+  //               _messangerKey.currentState.showSnackBar(
+  //                   SnackBar(content: Text("From : $from, To : $to")));
+  //               print("From : $from, To : $to");
+  //               Navigator.pop(context);
+  //             },
+  //             onCancel: () => Navigator.pop(context),
+  //           ),
+  //         );
+  //       });
+  // }
 }
